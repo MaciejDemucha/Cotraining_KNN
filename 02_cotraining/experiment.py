2f320476c2d1165aecf4724d52572931ace3cf09
Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
2025-01-13 20:19:47 +0100

    Kacper/base_for_custom_cotraining (#1)
    * base implementation for custom cotraining
    
    * add file for experiment

---
+++ 02_cotraining/experiment.py
@@ -0,0 +1,10 @@
+# Pytania badawcze:
+# 1. Jaki wpływ ma wartość parametru p dla algorytmu co-trainingu wykorzys-
+# tującego klasyfikatory kNN? - razem
+# Planowane jest wykonanie procedury co-training dla różnych wartości parametrów:
+# – p metryki odległości Minkowskiego: (p=1, 2, 3, ∞). - Maciek
+# – Liczba sąsiadów (k=3, 5, 7). - Maciek
+# – Procent oznaczonych danych (10%, 20%, 30%). - Kacper
+# – Próg ufności dla pseudo-etykietowania (0,7, 0,8). - Kacper
+# Eksperymenty zostaną przeprowadzone z 10 losowymi podziałami danych,
+# aby zapewnić istotność statystyczną.
